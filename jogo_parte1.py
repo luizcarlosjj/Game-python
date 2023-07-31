bdef0a99dc6228707451a1c8037a13ec1e08931a
Luiz Carlos <dia.a.diajj@gmail.com>
2023-07-30 22:25:25 -0300

    comandos commit

--- jogo_parte1.py
+++ jogo_parte1.py
@@ -2,6 +2,7 @@ import pygame
 pygame.init() # iniciando a lib pygame
 x = 400
 y = 300
+velocidade = 10
 
 janela = pygame.display.set_mode((800,600)) # criando e dando tamanho a tela
 pygame.display.set_caption("Criando um jogo com python") # dando nome a tela
@@ -15,11 +16,17 @@ while janela_aberta :
         if event.type == pygame.QUIT:
             janela_aberta = False
 
-        comandos = pygame.key.get_pressed()
-        if comandos[pygame.K_UP]:
-            y
+    comandos = pygame.key.get_pressed()
+    if comandos[pygame.K_UP]:
+        y-= velocidade
+    if comandos[pygame.K_DOWN]:
+        y+= velocidade
+    if comandos[pygame.K_RIGHT]:
+        x+= velocidade
+    if comandos[pygame.K_LEFT]:
+        x-= velocidade
 
-
+    janela.fill((0,0,0))
     pygame.draw.circle(janela, (0,255,0),(x,y),50)
     pygame.display.update()
 
