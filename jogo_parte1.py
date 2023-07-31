a4302c0b92536f7a10ce3543b1918367dad1bc3f
Luiz Carlos <dia.a.diajj@gmail.com>
2023-07-30 22:10:26 -0300

    update

--- jogo_parte1.py
+++ jogo_parte1.py
@@ -1,5 +1,7 @@
 import pygame 
 pygame.init() # iniciando a lib pygame
+x = 400
+y = 300
 
 janela = pygame.display.set_mode((800,600)) # criando e dando tamanho a tela
 pygame.display.set_caption("Criando um jogo com python") # dando nome a tela
@@ -7,9 +9,20 @@ pygame.display.set_caption("Criando um jogo com python") # dando nome a tela
 # Abrir e fechar tela em um programa Python.
 janela_aberta = True
 while janela_aberta :
+    pygame.time.delay(50)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             janela_aberta = False
 
+        comandos = pygame.key.get_pressed()
+        if comandos[pygame.K_UP]:
+            y
+
+
+    pygame.draw.circle(janela, (0,255,0),(x,y),50)
+    pygame.display.update()
+
+    # onde , cor , posição, arredondamento
 # Encerrar o jogo
 pygame.quit()
